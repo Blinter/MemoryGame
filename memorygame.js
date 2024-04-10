51717bcb48a1c5729e1fd1fd6890569e7ef494e0
Blinter <90986012+Blinter@users.noreply.github.com>
2024-04-10 13:37:54 -0400

    Add documentation, fix function naming

--- memorygame.js
+++ memorygame.js
@@ -11,12 +11,12 @@ document.addEventListener("DOMContentLoaded", () => {
         if (e.target.className === "shownitem" && !e.target.getAttribute("id")) {
             if (disablemoreclicks)
                 return;
-            UpdateCurrentScore();
+            updateCurrentScore();
             clickcount++;
             e.target.setAttribute("id", "gameitemshown");
             if (clickcount === 2) {
                 clickcount = 0;
-                CompareSelected();
+                compareSelected();
             }
         }
         if (e.target.tagName === "INPUT" && e.target.className === "startbutton") {
@@ -43,6 +43,12 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".currentGameStatistics").setAttribute("id", "currentGameStatisticsHidden");
     generateMainMenu();
 });
+/**
+* Clears any game objects currently showing and appends new cards to the Game Panel.
+* Queries the tenor v2 API, shuffles the results and creates the game objects for the user.
+* @param {number} itemsAmount - Generate game objects based on number
+* @return {void}
+*/
 function generateGameObjects(itemsAmount) {
     for (item of document.querySelectorAll(".gameitemwrapper"))
         item.remove();
@@ -52,7 +58,7 @@ function generateGameObjects(itemsAmount) {
     loadingLabel.innerText = "Loading images from API...";
     loadingLabel.setAttribute("id", "deleteme");
     document.querySelector(".gamepanel").appendChild(loadingLabel);
-    const newFun = setTimeout(() => {
+    setTimeout(() => {
         let i = 0;
         while (tenorimages.length != 0) {
             const url = tenorimages.pop();
@@ -66,22 +72,34 @@ function generateGameObjects(itemsAmount) {
         tenorimages = [];
     }, 1000);
 }
-var tenorimages = [];
+let tenorimages = [];
 let currentscore = 0;
 let currentbest = 0;
 let currentlevel = 0;
 let currentbestlevel = 0;
 let clickcount = 0;
 let disablemoreclicks = 0;
+/**
+* Resets global variables for current score and current level
+* @return {void}
+*/
 function resetcurrentScoreLevel() {
     currentscore = 0;
     currentlevel = 0;
 }
-function UpdateCurrentScore() {
+/**
+* Increments score by 1 and updates the DOM to show current score
+* @return {void}
+*/
+function updateCurrentScore() {
     currentscore++;
     document.querySelector(".currentscore").innerHTML = "Current<br />" + currentscore;
 }
-function UpdateBestScore() {
+/**
+* Updates the best score and level by comparing both, updates DOM, and stores into Local Storage
+* @return {void}
+*/
+function updateBestScore() {
     if (currentbest != 0 && currentbest < currentscore && currentbestlevel != 0 && currentlevel < currentbestlevel)
         return;
     if (currentbest === 0 || currentbest > currentscore)
@@ -89,47 +107,62 @@ function UpdateBestScore() {
     if (currentbestlevel === 0 || currentlevel > currentbestlevel)
         currentbestlevel = currentlevel;
     document.querySelector(".bestscore").innerHTML = "Best<br />" + currentbest;
-
     //Local Storage
     const saved = [];
     saved.push({ bestlevel: currentbestlevel, bestscore: currentbest });
     localStorage.setItem("memorygame", JSON.stringify(saved));
 }
+/**
+* Closes the start menu by deleting Start Menu elements from the DOM
+* @return {void}
+*/
 function closeStartMenu() {
     document.getElementsByClassName("gameIntro")[0].remove();
     document.getElementsByClassName("gameStatistics")[0].remove();
 }
-function CompareSelected() {
+/**
+* Searches the DOM for selected cards and determines if they are a match.
+* If they are not a match, they will automatically flip back after 1 second.
+* @return {void}
+*/
+function compareSelected() {
     let selectedCards = document.querySelectorAll("#gameitemshown");
     let selecteditems = new Set();
-    let currentsize = selecteditems.size;
     for (item of selectedCards)
         selecteditems.add(item.getAttribute("data-label").substring(4).substring(0, item.getAttribute("data-label").length - 5));
     if (selecteditems.size === 1) {
         for (item of document.querySelectorAll("#gameitemshown"))
             item.setAttribute("id", "gameitemcompleted");
-        CheckGameCompletion();
+        checkGameCompletion();
         selecteditems.clear();
         return;
     }
     for (item of document.querySelectorAll("#gameitemshown")) {
         item.setAttribute("id", "pendingreset");
         disablemoreclicks = 1;
-        const newFunct = setTimeout((item) => { item.removeAttribute("id"); disablemoreclicks = 0; }, 1000, item);
+        setTimeout((item) => { item.removeAttribute("id"); disablemoreclicks = 0; }, 1000, item);
     }
     selecteditems.clear();
 }
-function CheckGameCompletion() {
+/**
+* Searches the DOM for unsolved cards
+* If there are no remaining cards, the game will completed.
+* @return {void}
+*/
+function checkGameCompletion() {
     if (document.querySelectorAll("[id='pendingreset'").length != 0 || document.querySelectorAll("#gameitemshown").length != 0)
         return;
     for (item of document.getElementsByClassName("shownitem"))
         if (!item.getAttribute("id"))
             return;
     document.querySelector("#currentGameWonHidden").setAttribute("id", "currentGameWonShown");
-    UpdateBestScore();
+    updateBestScore();
     regenWinElement();
 }
-
+/**
+* Generate the elements to show the user they have won the game and update all game statistics.
+* @return {void}
+*/
 function regenWinElement() {
     document.querySelector(".currentGameWon").innerHTML = "<h1 class=\"currentGameWonMsg\">YOU WON!</h1><br />" +
         "DIFFICULTY: " + currentlevel +
@@ -141,6 +174,10 @@ function regenWinElement() {
     newButton.setAttribute("value", "MAIN MENU");
     document.querySelector(".currentGameWon").appendChild(newButton);
 }
+/**
+* Generate elements required to display the difficulty level form and game statistics.
+* @return {void}
+*/
 function generateMainMenu() {
     const gameIntro1 = document.createElement("input");
     gameIntro1.setAttribute("type", "button");
@@ -176,6 +213,14 @@ function generateMainMenu() {
     gameOutro3.className = "gameStatistics";
     document.querySelector("html").appendChild(gameOutro3);
 }
+/**
+* Generate a card based on an index, assign an image to show for the card, and apply a suffix as a unique identifier for matches.
+* Function is generally called twice with unique suffixes.
+* @param {number} count - An index to uniquely identify the card
+* @param {URL} url - A URL of an image to display when the card is chosen
+* @param {string} suffix - An additional string to add at the end of each item to identify the card
+* @return {Element}
+*/
 function generateCard(count, url, suffix) {
     const templateA = document.createElement("div");
     templateA.className = "gameitemwrapper";
@@ -194,10 +239,20 @@ function generateCard(count, url, suffix) {
     templateA.appendChild(templateB);
     return templateA;
 }
+/**
+* Append an array of HTML objects to the Game Panel.
+* @param {array} cardArray - An Array containing the HTML objects that have been generated
+* @return {void}
+*/
 function populateGameObjects(cardArray) {
     for (item of cardArray)
         document.querySelector(".gamepanel").appendChild(item);
 }
+/**
+* Shuffle an Array.
+* @param {array} array - An Array containing items to be shuffled
+* @return {void}
+*/
 function shuffle(array) {
     let currentIndex = array.length;
     while (currentIndex != 0) {
@@ -207,6 +262,13 @@ function shuffle(array) {
             [array[randomIndex], array[currentIndex]];
     }
 }
+/**
+* Async function to call a URL with a callback function once completed.
+* Handles the HTTP request.
+* @param {URL} theUrl - A HTTP link to retrieve data
+* @param {function} callback - A function to call when the Async task is completed
+* @return {void}
+*/
 function httpGetAsync(theUrl, callback) {
     var xmlHttp = new XMLHttpRequest();
     xmlHttp.onreadystatechange = function () {
@@ -217,12 +279,23 @@ function httpGetAsync(theUrl, callback) {
     xmlHttp.send(null);
     return;
 }
+/**
+* A Function used as a callback to return the contents of httpGetAsync.
+* Results are pushed to a global array.
+* @param {string} responsetext - A JSON string returned from the server
+*/
 function tenorCallback_search(responsetext) {
     const ResultsTest = JSON.parse(responsetext)["results"];
     for (item of ResultsTest)
         tenorimages.push(item["media_formats"]["gif"]["url"]);
     return;
 }
+/**
+* Accesses the Tenor V2 API and query for unique images to be used for game objects.
+* Results are stored in a global object.
+* @param {number} amt - The amount of unique GIF's to retrieve
+* @return {void}
+*/
 function grabTenorDatabase(amt) {
     let apikey = "AIzaSyB1WKQJpvPVYj3czgkrYKlaezhf009xlik";
     let clientkey = "MemoryGameDevelopment";
